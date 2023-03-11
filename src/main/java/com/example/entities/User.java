c6f7a786c881965e02ba203923bd0a53954aec71
Abdullah <abdullah.soft4021@gmail.com>
2023-03-11 14:50:34 +0530

    first commit

---
+++ src/main/java/com/example/entities/User.java
@@ -0,0 +1,69 @@
+package com.example.entities;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_table")
+@DynamicUpdate
+public class User {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "age")
+	private Integer age;
+
+	@Column(name = "date_of_birth")
+	private LocalDate dob;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", dob=" + dob + "]";
+	}
+}
